0fc2b9fa0cdbbe14d2acdaf41c7b7159d54f6a9e
alexpurhalo <alexpurhalo@gmail.com>
2016-09-29 16:53:13 -0400

    integration-spec

--- lib/app.rb
+++ lib/app.rb
@@ -2,7 +2,12 @@ module API
   class App < Grape::API
     resource :files do
       post do
+        params[:file].delete :file
+        asset = Asset.new params[:file]
+        asset.save
 
+        asset
+        asset.inspect
       end
     end
   end
--- test/integration/upload_file_test.rb
+++ test/integration/upload_file_test.rb
@@ -1,4 +1,5 @@
 require 'test_helper'
+require 'models/asset'
 require 'app'
 
 describe 'Upload a file' do
@@ -8,16 +9,22 @@ describe 'Upload a file' do
     API::App
   end
 
-  it 'uploads a file' do
+  before do
     file_path = fixture_path 'zip.zip'
 
     post '/files', {
         file: {
-            title: 'My first Zip File',
+            title: 'My First Zip File',
             file: Rack::Test::UploadedFile.new(file_path, 'application/zip', true)
         }
     }
+  end
 
+  it 'uploads a file' do
     last_response.status.must_equal 201
+  end
+
+  it 'retrieves the content for the new file' do
+    last_response.body.must_include 'My First Zip File'
   end
 end
